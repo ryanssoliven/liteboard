d7d8903dc031114980e0a41850765d0cc6db5dc9
ryanssoliven <58400550+ryanssoliven@users.noreply.github.com>
2019-12-01 15:23:15 -0500

    Update settings.py

--- liteboard/settings.py
+++ liteboard/settings.py
@@ -73,15 +73,15 @@ WSGI_APPLICATION = 'liteboard.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
+#if DEBUG:
+#    DATABASES = {
+#        'default': {
+#           'ENGINE': 'django.db.backends.sqlite3',
+#           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#       }
+#   }
+#else:
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'liteboarddb',
