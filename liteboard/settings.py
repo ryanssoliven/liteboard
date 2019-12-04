0e814eeeb96f33d37f1a2f26b7dcfee30288e9ae
ryanssoliven <58400550+ryanssoliven@users.noreply.github.com>
2019-12-04 16:07:08 -0500

    Update settings.py

--- liteboard/settings.py
+++ liteboard/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
     ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
